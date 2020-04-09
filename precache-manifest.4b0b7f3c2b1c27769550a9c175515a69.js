d4d54af9f565d16bd9f043fe25789ce27d423e68
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-09 16:43:45 +0800

    Deploy wankangk1/wankangk1.github.io to github.com/wankangk1/wankangk1.github.io.git:master

---
+++ precache-manifest.4b0b7f3c2b1c27769550a9c175515a69.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24e77e7bda7186796a35fb41be82a8fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0acd8ff153ba5ac42d5a",
+    "url": "/static/css/2.278a8f19.chunk.css"
+  },
+  {
+    "revision": "002e5bb8d9ed5eb335ee",
+    "url": "/static/css/main.8ec0ebe1.chunk.css"
+  },
+  {
+    "revision": "0acd8ff153ba5ac42d5a",
+    "url": "/static/js/2.860c7183.chunk.js"
+  },
+  {
+    "revision": "5c1c086ab1b20e3f620bdd8ace03f9d4",
+    "url": "/static/js/2.860c7183.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "002e5bb8d9ed5eb335ee",
+    "url": "/static/js/main.9b5479f9.chunk.js"
+  },
+  {
+    "revision": "228fa098814da32aa5c2",
+    "url": "/static/js/runtime-main.e1daef6b.js"
+  },
+  {
+    "revision": "b9ee665c2dea1b06e7fa5419336e39e0",
+    "url": "/static/media/banner.b9ee665c.jpg"
+  }
+]);
