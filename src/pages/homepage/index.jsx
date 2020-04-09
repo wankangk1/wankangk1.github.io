0bc492ee13213816276cca88a4fe6a7d9e35a283
wank <1521112991@qq.com>
2020-04-09 21:35:33 +0800

    dd

--- src/pages/homepage/index.jsx
+++ src/pages/homepage/index.jsx
@@ -1,44 +1,26 @@
 import React from "react"
-import {Layout} from "antd"
-import {Route, Switch, Redirect} from "react-router-dom"
+import { Row, Col } from "antd"
+import { Route, Switch, Redirect } from "react-router-dom"
 import HomeLeft from "./left"
 import Blog from "../blog"
 import Intro from "../intro"
 import "./index.less"
-const {Content, Sider} = Layout
-
+// 响应式
+const siderLayout = { xxl: 4, xl: 5, lg: 5, sm: 0, xs: 0 }
+const contentLayout = { xxl: 20, xl: 19, lg: 19, sm: 24, xs: 24 }
 export default (props) => {
   return (
-    <Layout className="homepage">
-      <Sider
-        breakpoint="10000"
-        theme="light"
-        collapsedWidth="0"
-        className="homepage-slider"
-        zeroWidthTriggerStyle={{
-          zIndex: 999,
-          top: 12,
-          right: -46,
-          borderRadius: 4,
-          color: "#fff",
-          backgroundColor: "rgba(128, 136, 144, 0.5)",
-        }}
-        onBreakpoint={(broken) => {
-          console.log(broken)
-        }}
-        onCollapse={(collapsed, type) => {
-          console.log(collapsed, type)
-        }}
-      >
+    <Row className="homepage">
+      <Col {...siderLayout} className="left-col">
         <HomeLeft />
-      </Sider>
-      <Content>
+      </Col>
+      <Col {...contentLayout}>
         <Switch>
           <Route path="/intro" component={Intro} />
           <Route path="/blog" component={Blog} />
           <Redirect to="/intro"></Redirect>
         </Switch>
-      </Content>
-    </Layout>
+      </Col>
+    </Row>
   )
 }
